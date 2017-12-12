a41ea735364c735d5e20fe7d5492e1524eb1a1e5
Timur Catakli <timurcatakli@gmail.com>
2017-12-12 10:33:19 -0800

    refactor code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { setSearchTerm } from '../actionCreators'
+
 const propTypes = {
   searchTerm: PropTypes.string,
   handleSearchTermChange: PropTypes.func
@@ -17,22 +16,10 @@ class Home extends Component {
         <Link to={'/aboutus'}>AboutUs</Link>
         -
         <Link to={'/dashboard'}>dashboard</Link>
-        -
-        {this.props.searchTerm} -
-        <button onClick={() => this.props.handleSearchTermChange(Math.random())}>
-          Change Search Term
-        </button>
       </p>
     )
   }
 }
 
-const mapStateToProps = state => ({ searchTerm: state.searchTerm })
-const mapDispatchToProps = dispatch => ({
-  handleSearchTermChange(value) {
-    dispatch(setSearchTerm(value))
-  }
-})
-
 Home.propTypes = propTypes
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
