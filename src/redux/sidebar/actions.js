6209bc0eb03063b87b4abdf46d9115f128e59752
Timur Catakli <timurcatakli@gmail.com>
2017-12-12 09:04:51 -0800

    refactor reducers

--- src/redux/sidebar/actions.js
+++ src/redux/sidebar/actions.js
@@ -1,8 +1,8 @@
 const actions = {
-  TOGGLE_SIDEBAR: 'TOGGLE_SIDEBAR',
-  toggleSidebar: status => ({
-    type: actions.TOGGLE_SIDEBAR,
-    payload: { status }
+  SIDEBAR_TOGGLE_REQUESTED: 'SIDEBAR_TOGGLE_REQUESTED',
+  toggleSidebar: () => ({
+    type: actions.SIDEBAR_TOGGLE_REQUESTED,
+    payload: {}
   })
 }
 export default actions
--- src/redux/sidebar/reducers.js
+++ src/redux/sidebar/reducers.js
@@ -2,13 +2,12 @@ import { Map } from 'immutable'
 import actions from './actions'
 
 const initState = new Map({
-  loading: false,
   collapsed: true
 })
 
 export default function reducer(state = initState, action) {
   switch (action.type) {
-  case actions.TOGGLE_SIDEBAR:
+  case actions.SIDEBAR_TOGGLE_REQUESTED:
     const currentState = state.get('collapsed')
     return state.set('collapsed', !currentState)
   default:
